9a1674750321e68d341454314dbc6a163cb56c9f
lanpoo <1195472620@qq.com>
2021-03-25 00:50:26 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <iostream>
+#include <set>
+using namespace std;
+
+
+class Student{
+public:
+    
+};
+int main()
+{
+    cout << "insert:" << endl;
+    set<testDemo> s1;
+    testDemo t1(1, "wjh");
+    s1.insert(t1);
+    cout << "emplace:" << endl;
+    set<testDemo> s2;
+    s2.emplace(2, "wjh");
+    return 0;
+}
